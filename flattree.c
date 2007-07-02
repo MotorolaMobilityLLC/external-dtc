msm8916-mainline/lk2nd

commit 0738774fcc34b78a708a127fb4b2f193d08c94ba

dtc: Accept NOPs in dtb input regardless of version
Since Milton's patch, dtc will accept (and, correctly, ignore) NOP
tags when given dtb input v16 or later.  However, although NOPs
weren't defined in earlier versions, they're not ambiguous, so should
be accepted there as well.  This patch does so, printing a mere
warning when finding NOPs in a too-early dtb version.

Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

flattree.c
@@ -776,10 +776,12 @@ static struct node *unflatten_tree(struct inbuf *dtbuf,
 			break;
 
 		case OF_DT_NOP:
-			if (flags & FTF_NOPS)
+			if (!(flags & FTF_NOPS))
+				fprintf(stderr, "Warning: NOP tag found in flat tree"
+					" version <16\n");
 				break;
 
-			die("OF_DT_NOP in device tree blob\n");
+			/* Ignore */
 			break;
 
 		default:

MotorolaMobilityLLC/external-dtc

commit 2cc9f71de0235d23db60e6dec62a16edc7753bf7

dtc: Remove bogus break statement
Commit 0738774fcc34b78a708a127fb4b2f193d08c94ba introduced some
incorrect indentation / bracketing in unflatten_tree().  By luck, the
extra break statement intended to be within an if block, but actually
afterwards has no semantic effect.  Still, this patch gets rid of it
for cleanliness.

Signed-off-by: David Gibson <david@gibson.dropbear.id.au>

flattree.c
@@ -779,7 +779,6 @@ static struct node *unflatten_tree(struct inbuf *dtbuf,
 			if (!(flags & FTF_NOPS))
 				fprintf(stderr, "Warning: NOP tag found in flat tree"
 					" version <16\n");
-				break;
 
 			/* Ignore */
 			break;